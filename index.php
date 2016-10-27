866f2be15e5a4fcef8a347b6444b8a179a059881
alexvahrushev92 <gnpr11@gmail.com>
2016-10-27 23:52:14 +0100

    hello world

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Deployment Test</title>
+</head>
+<body>
+<H1> XERES</H1>
+<H2> we deployed database</H2>
+<H3> testing database</H3>
+<H3> connecting...</H3>
+</body>
+</html>
