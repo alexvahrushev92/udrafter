0cbde613d56c53b23559ac15413c63c286fc25a7
alexvahrushev92 <gnpr11@gmail.com>
2016-10-28 01:23:51 +0100

    phpmyadmin - commit

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'udrafter.database.windows.net';
+$cfg['Servers'][$i]['host'] = 'udrafter.database.windows.net,1433';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
