3446714392e4edb3b550a65987f437224358d349
alexvahrushev92 <gnpr11@gmail.com>
2016-11-10 16:51:50 +0000

    testing php on web app

--- index.php
+++ index.php
@@ -8,7 +8,20 @@
 
 <H2> we deployed database</H2>
 <br/>
+<?php
+$servername = "eu-cdbr-azure-west-a.cloudapp.net";
+$username = "b6ad17a3124efa";
+$password = "721ca477";
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
 </body>
 </html>
 
