6d9199b9089322b52940716c932f4e019ec19dd4
alexvahrushev92 <gnpr11@gmail.com>
2016-11-10 13:31:10 +0000

    php $conn file to connect to database manually

--- db/conn.php
+++ db/conn.php
@@ -1,16 +1,4 @@
 <?php
-$servername = "udrafter.database.windows.net";
-$username = "dbadmin";
-$password = "Pa$$w0rd";
+Server: udrafter.database.windows.net,1433 \r\nSQL Database: udrafter\r\nUser Name: dbadmin\r\n\r\nPHP Data Objects(PDO) Sample Code:\r\n\r\ntry {\r\n   $conn = new PDO ( \"sqlsrv:server = tcp:udrafter.database.windows.net,1433; Database = udrafter\", \"dbadmin\", \"Pa$$w0rd\");\r\n    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );\r\n}\r\ncatch ( PDOException $e ) {\r\n   print( \"Error connecting to SQL Server.\" );\r\n   die(print_r($e));\r\n}\r\n\rSQL Server Extension Sample Code:\r\n\r\n$connectionInfo = array(\"UID\" => \"dbadmin@udrafter\", \"pwd\" => \"Pa$$w0rd\", \"Database\" => \"udrafter\", \"LoginTimeout\" => 30, \"Encrypt\" => 1, \"TrustServerCertificate\" => 0);\r\n$serverName = \"tcp:udrafter.database.windows.net,1433\";\r\n$conn = sqlsrv_connect($serverName, $connectionInfo);
 
-try {
-        $conn = new PDO("mysql:host=$servername;dbname=udrafter", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-    }
-catch(PDOException $e)
-    {
-        echo "Connection failed: " . $e->getMessage();
-    }
 ?>
