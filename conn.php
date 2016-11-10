1caf8a20009396583b1feeaf43cfec162a7a9698
alexvahrushev92 <gnpr11@gmail.com>
2016-11-10 13:15:38 +0000

    php $conn file to connect to database manually

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "udrafter.database.windows.net";
+$username = "dbadmin";
+$password = "Pa$$w0rd";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
