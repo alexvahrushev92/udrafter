605f145221dae8ddcdde6bc543badc104d0d5999
alexvahrushev92 <gnpr11@gmail.com>
2016-11-10 14:59:14 +0000

    php $conn file to connect to database manually

--- index.php
+++ index.php
@@ -20,14 +20,12 @@ $servername = "eu-cdbr-azure-west-a.cloudapp.net";
 $username = "b80f75f9cd38fe";
 $password = "c069e447";
 
-try {
-        $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-    }
-catch(PDOException $e)
-    {
-        echo "Connection failed: " . $e->getMessage();
-    }
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
